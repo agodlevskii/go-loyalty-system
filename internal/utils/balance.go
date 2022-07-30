fc7c734a8af317494d394c38df8c96bcc99130c1
Andrei Godlevskii <andrei.godlevskii@icloud.com>
2022-07-30 19:46:00 +0300

    Balance
    * Fix the edge-case with non-existing balance.

--- internal/utils/balance.go
+++ internal/utils/balance.go
@@ -1,12 +1,19 @@
 package utils
 
 import (
+	"database/sql"
+	"errors"
 	"go-loyalty-system/internal/models"
 	"go-loyalty-system/internal/storage"
 )
 
 func UpdateBalanceWithAccrual(bs storage.BalanceStorage, user string, accrual float64) (models.Balance, error) {
 	b, err := bs.Get(user)
+	if errors.Is(err, sql.ErrNoRows) {
+		b = models.NewBalance(user)
+		err = bs.Set(b)
+	}
+
 	if err != nil {
 		return b, err
 	}
